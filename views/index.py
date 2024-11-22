1a590f96406069270afc4582ce3ada82d30fd51e
monkeyclash <monkeyclash15@gmail.com>
2024-11-21 16:53:15 -0800

    added more words and submit button updated

--- views/index.py
+++ views/index.py
@@ -1,5 +1,6 @@
 import flet as ft
 from modules import mytimer
+from modules import my_module
 from modules import spell_server as server
 def IndexView(page:ft.Page, params):
     def timer_end(e):
@@ -27,18 +28,36 @@ def IndexView(page:ft.Page, params):
         page.update()
     def clear_top_buttons(e):
           for top_btn in top_row_buttons.controls:
-              x = top_btn.text
-              print(x)
-              if x !=" ":
-                  for bottom_btn in bottom_row_buttons.controls:
-                      if bottom_btn.text == " " and bottom_btn.data== x:
-                          print("found")
-                          bottom_btn.text=x
-                          bottom_btn.disabled=False
-                          top_btn.text =" "
-                          break
+              top_btn.text= " "
+          for bottom_btn in bottom_row_buttons.controls:
+              bottom_btn.text= bottom_btn.data
+              bottom_btn.disabled = False
           page.update()
+    def submit_click(e):
+        word = ""
+        for top_btn in top_row_buttons.controls:
+            if top_btn.text != " ":
+                word+=top_btn.text
+            else:
+                break
+        print(word)
+        is_valid = True
+        if len(word)<3:
+            is_valid=False
+            print("len less than 3")
+        if word in user_words:
+            is_valid=False
+            print("is in userword")
+        if is_valid and word not in all_words:
+            is_valid=False
+            print("not in all word")
+        if is_valid:
+            user_words.append(word)
 
+            user_words_textbox.value+= word + " "
+            page.update()
+        else:
+            print("invalid")
 
     def new_round(word):
         top_row_buttons.controls.clear()
@@ -64,25 +83,29 @@ def IndexView(page:ft.Page, params):
     game_state = game_client.get_game_state()
     timer = mytimer.Countdown(game_state["time_remaining"], timer_end)
     print(game_state)
-    #word = game_state["current_word"].upper()
-    word = "LOOP"
+    word = game_state["current_word"].upper()
+    #word = "LOOP"
     print(word)
     top_row_buttons = ft.Row(spacing=10,
         alignment=ft.MainAxisAlignment.CENTER)
     bottom_row_buttons = ft.Row(spacing=10,
         alignment=ft.MainAxisAlignment.CENTER)
-
     third_row_buttons = ft.Row(spacing=200,
         alignment=ft.MainAxisAlignment.CENTER)
-    submit_button = ft.OutlinedButton("Submit")
+    submit_button = ft.OutlinedButton("Submit" ,on_click=submit_click)
     clear_button = ft.OutlinedButton("Clear" ,on_click=clear_top_buttons)
 
     third_row_buttons.controls.append(submit_button)
     third_row_buttons.controls.append(clear_button)
+    user_words_textbox=ft.Text("")
     new_round(word)
+    all_words=my_module.GetAllWords("data/3_letter_plus_words.txt")
+    print(all_words[1:10])
+    print("ten")
+    user_words=[]
     page.views.append(ft.View(
         "/",
-        [appbar, timer, top_row_buttons, bottom_row_buttons,third_row_buttons],
+        [appbar, timer, top_row_buttons, bottom_row_buttons,third_row_buttons,user_words_textbox],
 
 
     )
