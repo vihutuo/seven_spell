5a60ba4dd85fd7a3211abdb42c7f8b069741a5bf
sdasd <vpaira@gmail.com>
2024-11-26 22:28:34 +0530

    change title

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from views.question import QuestionView
 from views.simple_view import SimpleView
 
 def main(page: ft.Page):
-  page.title = "Flet Template"
+  page.title = "Seven Spell"
   #page.theme_mode = "light"
   page.theme = ft.Theme(color_scheme_seed="#FFCD5758")
   page.fonts = {
@@ -30,5 +30,5 @@ def main(page: ft.Page):
   page.on_route_change = route_change
   page.on_view_pop = view_pop
   page.go(page.route)
-ft.app(target=main)
-#ft.app(target=main,  view=ft.AppView.WEB_BROWSER)
+#ft.app(target=main)
+ft.app(target=main,  view=ft.AppView.WEB_BROWSER)
