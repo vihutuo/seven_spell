23aa38d22a5f36e661494072b20bfa17a15c399e
monkeyclash <monkeyclash15@gmail.com>
2024-11-29 02:45:09 -0800

    everything done

--- modules/my_module.py
+++ modules/my_module.py
@@ -1,7 +1,7 @@
 import random
 import csv
 import random
-
+from itertools import permutations
 
 def ShuffleString(s):
     L1 = list(s)
@@ -26,3 +26,16 @@ def GetAllWords(file_name):
     with open(file_name, "r") as f:
         L1 = f.read().upper().splitlines()
     return L1
+
+def generate_valid_words(letters, word_set, min_length=3):
+    # List to store valid words
+    valid_words = []
+
+    # Iterate over all possible word lengths
+    for length in range(min_length, len(letters) + 1):
+        for perm in permutations(letters, length):
+            candidate_word = ''.join(perm)
+            if candidate_word in word_set:
+                valid_words.append(candidate_word)  # Add valid word to the list
+
+    return valid_words  # Return the list of valid words
